077c0fce2e64ec628255f72017946961dce4e98b
Sudipta Ghoshal <ghoshalsudipta36@gmail.com>
2026-07-27 15:49:55 +0530

    class 02: list , append , insert , for loop

--- classes/class-02/menu_list.py
+++ classes/class-02/menu_list.py
@@ -1,8 +1,24 @@
 # Since we dont have navbar, so we have to create Menu Driven Program using List
 # Declaring a list to store multiple data
-data_list: list = []
+data_list: list = ['24', '20', '22', '22.5', '23.33' ,'sudipta', '24', '25', '20', 'apple', '30', '32', '50']
+# data_list: list = [ 'apple', 'sudipta']
 # data_list: list = list()
 print(type(data_list))
+
+# num_list = []
+# def number_list(itm_list):
+#     for itme in itm_list:
+#         try:
+#             num = float(item)
+#             if(num.is_integer()):
+#                 num_list.append(int(num))
+#             else:
+#                 num_list.append(num)
+#         except:
+#             pass
+
+# print(20 == 20.0)
+# print(20.0.is_integer())
 
 while True:   # Infinite loop boolean True
     print("\n------ MENU ------")
@@ -10,24 +26,25 @@ while True:   # Infinite loop boolean True
     print("2. Display data")
     print("3. Insert item at a location")
     print("4. Addition of all integer values in the list")
-    print("5. Exit")
+    print("5. Search an numeric value in the list")
+    print("6. Exit")
 
     choice: int = int(input("Enter your choice: "))
 
     if choice == 1:
         item: str = input("Enter data to add: ")
-        data_list.append(item)                  # Item added to the end of the list
+        data_list.append(item)                  # Item added to the end of the list              
         print("Data added successfully.")
 
     elif choice == 2:
         if len(data_list) == 0:                 # Consider no item in the list but user want to display data, so how do we know if the list has element or not
             print("List is empty.")             # calculate the len()
         else:
-            # print("Current List:", data_list)
+            print("Current List:", data_list)
             # for item in data_list:
             #     print(item)
-            for item in range(0, len(data_list)):
-                print(f"Index {item}: {data_list[item]}")   # Displaying the index and the item in the list
+            # for item in range(0, len(data_list)):
+            #     print(f"Index {item}: {data_list[item]}")   # Displaying the index and the item in the list
 
     elif choice == 3:
         if len(data_list) == 0:
@@ -53,7 +70,46 @@ while True:   # Infinite loop boolean True
                 else:
                     print(f"The type of {item} is:", type(item))
             print("The sum of all integer values in the list is:", total)
+
     elif choice == 5:
+        if len(data_list) == 0:
+            print("List is empty. Add data first.")
+        else:
+            search_item = float(input("Enter the numeric value to search: "))
+            num_list = []
+            for item in data_list:
+                try:
+                    num = float(item)
+                    if num.is_integer():
+                        num_list.append(int(num))
+                    else:
+                        num_list.append(num)
+                except ValueError:
+                    pass
+
+            if len(num_list) == 0:
+                print('No number found')
+                break
+
+            if search_item in num_list:
+                print(f"Exact match found: {int(search_item) if search_item.is_integer() else search_item}")
+            else:
+                upper = search_item + 5
+                found = False
+
+                for num in num_list:
+                    if num > search_item and num <= upper:
+                        print(f"Number within +5 range: {num}")
+                        found = True
+
+                if not found:
+                    print("No match found")
+            # for item in data_list:
+            #     if int(item) == search_item:
+            #         print(f"{search_item} found in the list.")
+            #     else:
+            #         print(f"{search_item} not found in the list.")
+    elif choice == 6:
         print("Exiting program...")
         break   # Exit infinite loop
 
---
+++ practice/main.py
@@ -0,0 +1,28 @@
+# Python has the following data types built-in by default, in these categories:
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	    dict
+# Set Types:	    set, frozenset
+# Boolean Type:	    bool
+# Binary Types:	    bytes, bytearray, memoryview
+# None Type:	    None
+
+
+x = "Hello"
+x = 5
+x = 3.5
+x = 1j
+x = ["apple", 4, True, "apple"]
+x = ("apple", 4, True, "apple")
+x = range(6)
+x = {"name": "sudipta", "age": 26}
+x = {"apple", 5, False}
+x = True
+x = b"Hello"
+x = bytearray(5)
+x = memoryview(bytes(5))
+x = None
+
+print(x)
